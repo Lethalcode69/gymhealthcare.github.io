445833a784d044c149c26ddd997196e9d1183771
aman <lethalcode@yahoo.com>
2022-07-14 00:39:45 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// route b/w our home page and access details page
+import {Route, Routes} from 'react-router-dom';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import {Box} from '@mui/material';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
+  )
+}
+
+export default App
